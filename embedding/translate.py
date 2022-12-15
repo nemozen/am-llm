26632981701dbf7f1869d9f7c61011ca00208382
Nemo Semret <nemo@semret.org>
2022-12-15 22:39:30 +0300

    add translation

---
+++ embedding/translate.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import sys
+import six
+from google.cloud import translate_v2 as translate
+
+
+def translate_text(text, src_lang, dest_lang):
+    """Translates text into the target language.
+
+    Target must be an ISO 639-1 language code.
+    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+    """
+
+    translate_client = translate.Client()
+
+    if isinstance(text, six.binary_type):
+        text = text.decode("utf-8")
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(
+        text, target_language=dest_lang, source_language=src_lang)
+    return result["translatedText"]
+
+
+if __name__ == "__main__":
+    
+    for line in sys.stdin:
+        print(translate_text(line.strip(), "am", "en"))
