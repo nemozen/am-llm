01c72f6fe52448ffce93ac58f6768ba533cd2636
Nemo Semret <nemo@semret.org>
2022-12-20 10:15:09 +0300

    translate in batches instead of line by line
    to optimize throughput to google translate api

--- embedding/translate.py
+++ embedding/translate.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 
+import logging
 import six
 import sys
 from google.cloud import translate_v2 as translate
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stderr)
+logger.addHandler(handler)
+
+BATCH=100
 
 
 def translate_text(text, src_lang, dest_lang):
@@ -20,11 +29,22 @@ def translate_text(text, src_lang, dest_lang):
     # Text can also be a sequence of strings, in which case this method
     # will return a sequence of results for each text.
     result = translate_client.translate(
-        text, target_language=dest_lang, source_language=src_lang)
+        text, target_language=dest_lang, format_='text', source_language=src_lang)
     return result["translatedText"]
 
 
 if __name__ == "__main__":
 
+    batch = ''
+    linenum = 0
     for line in sys.stdin:
-        print(translate_text(line.strip(), "am", "en"))
+        linenum += 1
+        batch += line.strip()
+        batch += '\n'
+        if linenum%BATCH == 0:
+            logger.info("Line {}, translating batch size {}".format(
+                linenum, sys.getsizeof(batch)))
+            logger.debug(batch)
+            print(translate_text(batch, "am", "en"))
+            batch = ''
+    print(translate_text(batch, "am", "en"))
