910d310afc79cb9f87f41e54d6a569810934572d
Nemo Semret <nemo@semret.org>
2022-12-26 20:59:52 +0300

    filter for given unicode range, and allow xml as well as html files

--- embedding/amparser.py
+++ embedding/amparser.py
@@ -7,7 +7,8 @@ from html.parser import HTMLParser
 
 # whitespace, ascii punctuation, ge'ez punctuation, other punctuation, arabic numberals
 WORD_SEP=r"\s|[!\"#\$%&\'\(\)\*\+,-\./:;<=>\?@\[\\\]\^_`{\|}~]|[።፡፣፤፥፦፧፨፠]|[‘‚“”•…«»]|[0-9]"
-
+# ethiopic unicode: 1200–137F
+CHAR_RANGE=['ሀ', '፼']
 
 def get_html_files(dirname):
     for dirpath,dirs,files in os.walk(dirname):
@@ -15,7 +16,7 @@ def get_html_files(dirname):
             fpath = os.path.join(dirpath,fname)
             if not os.path.isfile(fpath):
                 continue
-            if fname.split('.')[-1] != 'html':
+            if fname.split('.')[-1] not in ['html','xml']:
                 continue
             yield fpath
 
@@ -24,7 +25,9 @@ class WordsFromHTML(HTMLParser):
     words = set()
 
     def handle_data(self, data):
-        self.words.update(re.split(WORD_SEP, data))
+        self.words.update(
+            filter(lambda w: len(w) and w[0] >= CHAR_RANGE[0] and w[0]<= CHAR_RANGE[1],
+                   re.split(WORD_SEP, data)))
 
 
 def get_file_text(fpath):
@@ -32,8 +35,8 @@ def get_file_text(fpath):
     for line in open(fpath):
         parser.feed(line)
     return parser.words
-    
-    
+
+
 if __name__ == "__main__":
     words = set()
     num_files = 0
