6806574867ef3c2ebd1f3417da52e5a851fc7245
Nemo Semret <nemo@semret.org>
2023-01-26 21:18:27 +0300

    attention model
    attention model impl
    support for multiple models
    use updated special tokens impl from embedding module

--- models/am2en.py
+++ models/am2en.py
@@ -9,20 +9,19 @@ from keras.callbacks import CSVLogger
 
 from embedding.ambert_embedding import AmBert
 from embedding.bert_embedding import Bert
-from embedding.bert_embedding import PAD, PAD_ID, BERT_PAD_ID
+from embedding.bert_embedding import SPECIAL_TOKENS
 
 
 BATCH_SIZE=32
 INPUT_WIDTH=10  # max length in tokens per row of input
 OUTPUT_WIDTH=32  # max length in tokens per row of output
-MODEL_NAME="am2en_idp"
-OUTPUT_TOKENS_TO_FILTER=[PAD]
+OUTPUT_TOKENS_TO_FILTER=["[PAD]"]
 
 logger = logging.getLogger("am2en")
 handler = logging.StreamHandler(sys.stderr)
 logger.addHandler(handler)
 # Adjust logging level for this module
-# logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 
 ambert = AmBert()
@@ -62,10 +61,10 @@ def load_training_data(xfile, yfile):
 
     # make rectangular
     for row in x:
-        row += [PAD_ID]*(INPUT_WIDTH-len(row))
+        row += [SPECIAL_TOKENS.get("[PAD]")[1]]*(INPUT_WIDTH-len(row))
 
     for row in y:
-        row += [BERT_PAD_ID]*(OUTPUT_WIDTH-len(row))
+        row += [SPECIAL_TOKENS.get("[PAD]")[0]]*(OUTPUT_WIDTH-len(row))
 
     for i in range(len(y)):
         y[i] = bert.get_embedding_layer()(tf.convert_to_tensor(y[i]))
@@ -81,7 +80,34 @@ def load_training_data(xfile, yfile):
     return x,y
 
 
-def build_model(input_width, output_width):
+def build_attention_model(input_width, output_width):
+    input_layer =tf.keras.Input(batch_size=BATCH_SIZE, shape=(input_width,))
+    ambert_layer = ambert.get_embedding_layer(input_width)(input_layer)
+    # TODO: add position encoding to embedding output
+    attention_layers = []
+    for i in range(output_width):
+        if i == 0:
+            query_ = ambert_layer
+        else:
+            query_ = attention_layers[i-1]
+        query = tf.keras.layers.Dense(embedding_dims,
+                                      name="query{}".format(i))(query_)
+        value = tf.keras.layers.Dense(embedding_dims,
+                                      name="value{}".format(i))(ambert_layer)
+        key = tf.keras.layers.Dense(embedding_dims,
+                                    name="key{}".format(i))(ambert_layer)
+        attn = tf.keras.layers.Attention()([query, value, key])
+        attention_layers.append(attn)
+
+    attention = tf.keras.layers.Concatenate()(attention_layers)
+    reduced = tf.keras.layers.GlobalAveragePooling1D()(attention)
+    output_layer = tf.keras.layers.Reshape(
+        (output_width, embedding_dims))(reduced)
+    model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
+    return model
+
+
+def build_idp_model(input_width, output_width):
     '''Four layers: ambert embedding, flattening, dense layer, and output
     layer.  The AMBERT embedding is designed to be semantically
     related to the BERT english embedding used to decode the output,
@@ -100,25 +126,31 @@ def build_model(input_width, output_width):
     output_layer = tf.keras.layers.Reshape(
         (output_width, embedding_dims))(dense_layer)
     model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
-    # tf.keras.utils.plot_model(model, to_file='{}.png'.format(MODEL_NAME), show_shapes=True)
     return model
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Amharic to English translator.")
+    parser.add_argument("--model", type=str, default="attention",
+                        choices=["attention", "idp"], help="name of model to use")
     parser.add_argument("--train", type=int, default=0,
                         help="train model for this number of epochs")
     parser.add_argument("--predict", action='store_true', help="translate from stdin")
     args = parser.parse_args()
 
+    build_model_function = "build_{}_model".format(args.model)
+    model_name = "am2en_{}".format(args.model)
+
     strategy = tf.distribute.MultiWorkerMirroredStrategy()
     with strategy.scope():
 
-        model = build_model(INPUT_WIDTH, OUTPUT_WIDTH)
+        model = globals()[build_model_function](INPUT_WIDTH, OUTPUT_WIDTH)
+        tf.keras.utils.plot_model(model, to_file='{}.png'.format(model_name),
+                                  show_shapes=True)
 
         try:
-            model.load_weights('{}.ckpt'.format(MODEL_NAME))
-            logger.info("Loaded model weights from {}.ckpt".format(MODEL_NAME))
+            model.load_weights('{}.ckpt'.format(model_name))
+            logger.info("Loaded model weights from {}.ckpt".format(model_name))
         except tf.errors.NotFoundError as e:
             logger.info(e)
 
@@ -129,8 +161,8 @@ if __name__ == '__main__':
                     "../../Amharic-English-Machine-Translation-Corpus/new-en.txt")
             csv_logger = CSVLogger('log.csv', append=True, separator=';')
             model.fit(x, y, batch_size=BATCH_SIZE, epochs=args.train, verbose=True, callbacks=[csv_logger])
-            model.save_weights('{}.ckpt'.format(MODEL_NAME))
-            logger.info("Saved model weights to {}.ckpt".format(MODEL_NAME))
+            model.save_weights('{}.ckpt'.format(model_name))
+            logger.info("Saved model weights to {}.ckpt".format(model_name))
 
         if args.predict:
             for line in sys.stdin:
@@ -139,7 +171,7 @@ if __name__ == '__main__':
                 for i in range(1+int(len(v)/INPUT_WIDTH)):
                     x = v[i*INPUT_WIDTH:(i+1)*INPUT_WIDTH]
                     # pad to length INPUT_WIDTH
-                    x += [PAD_ID]*(INPUT_WIDTH-len(x))
+                    x += [SPECIAL_TOKENS.get("[PAD]")[1]]*(INPUT_WIDTH-len(x))
                     x = tf.convert_to_tensor([x], dtype=tf.float32)
                     logger.debug("Encoded input: {} {}".format(x.shape, x))
                     res = model(x)
